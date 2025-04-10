e26d6d8c8992f11b27cb20027ac1c8e2ab7cfbab
Rebeca Alves <m.rebeca@aluno.ifsp.edu.br>
2025-04-10 11:29:01 -0300

    Add files via upload

--- ex01.py
+++ ex01.py
@@ -1,26 +1,33 @@
-num1=int(input("digite um número: "))
-num2=int(input("digite um número: "))
-num3=int(input("digite um número: "))
-num4=int(input("digite um número: "))
-num5=int(input("digite um número: "))
+'''Elabore um programa que efetue a soma de todos os números ímpares que são
+múltiplos de 3 e que se encontram no intervalo de 1 a 500'''
 
-if num1>num2 and num1>num3 and num1>num4 and num1>num5:
-    print("O primeiro número digitado é o maior!")
+i=1
+soma=0
+impares=[]
+multiplos=[]
+
+while i <= 5:
+    if i % 3 == 0:
+        multiplos.append(i)
+    i+=1
+
+i=0
+par = 0
+while i < len(multiplos):
+    if multiplos[i]%2==0:
+        par+=1
+    else:
+        impares.append(multiplos[i])
+    i+=1
+
+soma = 0
+i=0
+
+while i < len(impares):
+    soma = impares[i]+soma
+    i+=1
+
+print(soma)
+
     
-elif num2>num3 and num2>num4 and num2>num5: 
-    print("O segundo número digitado é o maior!")
-    
-elif num3>num4 and num3>num5: 
-    print("O terceiro número digitado é o maior!")
 
-elif num4>num5:
-    print("O qurto número digitado é o maior!")
-    
-else: 
-    print("O quinto número digitado é o maior")
-
-
-
-    
-
-    
