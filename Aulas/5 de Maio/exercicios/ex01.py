a4f0b22c09d64cbd58c9894ff07c40d212b1c158
Rebeca Alves <m.rebeca@aluno.ifsp.edu.br>
2025-05-05 11:26:27 -0300

    Update ex01.py

--- Aulas/5 de Maio/exercicios/ex01.py
+++ Aulas/5 de Maio/exercicios/ex01.py
@@ -1,1 +1,13 @@
+'''Escreva um programa que remove a primeira ocorrência de uma letra de
+uma string. A string e a letra devem ser fornecidas pelo usuário.'''
+palavra=input("Digite uma palavra: ")
+letra_ser_removida=input("Digite uma letra a ser removida: ")
+palavra_nova = " "
+for letra in palavra:
+    if letra != letra_ser_removida: 
+        palavra_nova = palavra_nova + letra
+print(palavra_nova)
+
+
+
 
