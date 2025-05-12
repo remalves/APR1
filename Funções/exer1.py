ba2994c8818221896dae4bb7400541637ffd49d4
Rebeca Alves <m.rebeca@aluno.ifsp.edu.br>
2025-05-12 11:26:06 -0300

    Create exer1.py

---
+++ Funções/exer1.py
@@ -0,0 +1,20 @@
+def soma(n1,n2):
+    return n1+n2 #retorna um valor q é produzido dentro da função
+def subtrai(n1,n2):
+    res = n1-n2
+    return res
+def divide(n1,n2):
+    if n2!=0:
+        return n1/n2 
+
+#--MAIN--#
+num1=int(input('valor 1: '))
+num2=int(input('valor 2: '))
+
+result = soma(num1,num2)#chamando a função soma()
+print(f'soma= {result}')
+
+result = subtrai(num1,num2)
+print(f'subtração = {result}')
+
+divide(num1,num2)
